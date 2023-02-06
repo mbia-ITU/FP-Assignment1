bf343909de86ee60bfd3fde336082589562a16fd
Mikkel Bistrup Andersen <mbia@itu.dk>
2023-02-06 10:03:36 +0100

    changed != to <>

--- Exercises.fs
+++ Exercises.fs
@@ -5,7 +5,7 @@ let sqr x = x*x
 let pow x n = System.Math.Pow(x,n)
 
 //Exercise 1.3
-let rec sum n = if n != 0 then n+(sum(n-1)) else 0
+let rec sum n = if n <> 0 then n+(sum(n-1)) else 0
 
 //Exercise 1.4
 let rec fib n =
@@ -18,7 +18,7 @@ let rec fib n =
 let dup (s: string) = s+s
 
 //Exercise 1.6
-let rec dupn s n = if n != 0 then s+dupn (s) (i-1) else ""
+let rec dupn s n = if n <> 0 then s+dupn (s) (i-1) else ""
 
 //Exercise 1.7
-Let rec bin (n,k) = if n!=k ||k!=0 then bin(n-1,k-1) + bin(n-1,k) else 1
+Let rec bin (n,k) = if n<>k ||k<>0 then bin(n-1,k-1) + bin(n-1,k) else 1
